3901ac109833a58b21cd486bea6d8c7decf7dcec
Carovvel <caroline.van.velthoven@gmail.com>
2014-05-11 19:02:48 +0200

    Initial view controller creation
    Created different view controllers for screens.
    Added some to storyboard.

---
+++ RockFolio/RockFolio/Venues/VenuesViewController.h
@@ -0,0 +1,13 @@
+//
+//  VenuesViewController.h
+//  RockFolio
+//
+//  Created by Caroline Van Velthoven on 11/05/14.
+//  Copyright (c) 2014 Caroline Van Velthoven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VenuesViewController : UIViewController
+
+@end

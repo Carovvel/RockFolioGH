37b6e39d76abb95fcb5bfe356d7336a501f38ad8
Carovvel <caroline.van.velthoven@gmail.com>
2014-05-11 18:34:21 +0200

    Initialize
    Initial Project Creation

---
+++ RockFolio/RockFolio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RockFolio
+//
+//  Created by Caroline Van Velthoven on 11/05/14.
+//  Copyright (c) 2014 Caroline Van Velthoven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RockFolio/RockFolio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RockFolio
+//
+//  Created by Caroline Van Velthoven on 11/05/14.
+//  Copyright (c) 2014 Caroline Van Velthoven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

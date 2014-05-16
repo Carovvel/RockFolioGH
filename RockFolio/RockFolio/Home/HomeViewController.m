c26ea398dd90bb12d6308edbbece142099f20857
Carovvel <caroline.van.velthoven@gmail.com>
2014-05-16 20:44:06 +0200

    Revert a6f0d30..3333a4a
    This rolls back to commit a6f0d30fba2b05d0eb61d323ed5123a8622e9555.

--- RockFolio/RockFolio/Home/HomeViewController.m
+++ RockFolio/RockFolio/Home/HomeViewController.m
@@ -8,10 +8,8 @@
 
 #import "HomeViewController.h"
 #import "SWRevealViewController.h"
-#import "CustomSegue.h"
 
 @interface HomeViewController ()
-@property (strong, nonatomic) IBOutlet UIButton *HomeButtonSegue;
 
 @end
 
@@ -40,14 +38,15 @@
     // Dispose of any resources that can be recreated.
 }
 
-
+/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-// Prepare for the segue going forward
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    if([segue isKindOfClass:[CustomSegue class]]) {
-        // Set the start point for the animation to center of the button for the animation
-        ((CustomSegue *)segue).originatingPoint = self.HomeButtonSegue.center;
-    }
-}@end
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

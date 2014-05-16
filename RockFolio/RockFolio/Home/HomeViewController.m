3333a4a6cb7b50597b3dc34319d669ee8b7bb67f
Carovvel <caroline.van.velthoven@gmail.com>
2014-05-16 20:43:26 +0200

    Preparing custom segues
    Trying to create custom segues

--- RockFolio/RockFolio/Home/HomeViewController.m
+++ RockFolio/RockFolio/Home/HomeViewController.m
@@ -8,8 +8,10 @@
 
 #import "HomeViewController.h"
 #import "SWRevealViewController.h"
+#import "CustomSegue.h"
 
 @interface HomeViewController ()
+@property (strong, nonatomic) IBOutlet UIButton *HomeButtonSegue;
 
 @end
 
@@ -38,15 +40,14 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-@end
+// Prepare for the segue going forward
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if([segue isKindOfClass:[CustomSegue class]]) {
+        // Set the start point for the animation to center of the button for the animation
+        ((CustomSegue *)segue).originatingPoint = self.HomeButtonSegue.center;
+    }
+}@end

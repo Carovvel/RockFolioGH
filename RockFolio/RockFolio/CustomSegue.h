2a9a6d9136e25ee6739f2615976798b53d012b64
Carovvel <caroline.van.velthoven@gmail.com>
2014-05-17 17:40:48 +0200

    No big changes

---
+++ RockFolio/RockFolio/CustomSegue.h
@@ -0,0 +1,13 @@
+//
+//  CustomSegue.h
+//  RockFolio
+//
+//  Created by Caroline Van Velthoven on 17/05/14.
+//  Copyright (c) 2014 Caroline Van Velthoven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomSegue : UIStoryboardSegue
+
+@end
---
+++ RockFolio/RockFolio/CustomSegue.m
@@ -0,0 +1,30 @@
+//
+//  CustomSegue.m
+//  RockFolio
+//
+//  Created by Caroline Van Velthoven on 17/05/14.
+//  Copyright (c) 2014 Caroline Van Velthoven. All rights reserved.
+//
+
+#import "CustomSegue.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation CustomSegue
+/*
+- (void)perform{
+    UIViewController *srcViewController = (UIViewController *) self.sourceViewController;
+    UIViewController *destViewController = (UIViewController *) self.destinationViewController;
+    
+    CATransition *transition = [CATransition animation];
+    transition.duration = 0.3;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    transition.type = kCATransitionPush;
+    transition.subtype = kCATransitionFromRight;
+    [srcViewController.view.window.layer addAnimation:transition forKey:nil];
+    
+    [srcViewController presentViewController:destViewController animated:NO completion:nil];
+}
+
+
+*/
+@end
